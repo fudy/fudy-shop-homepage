e853a6029669ae531b42666a5eda52a882ff1495
Fudy <lifudong1@163.com>
2022-07-03 14:32:38 +0800

    init homepage project

---
+++ src/main/java/com/fudy/homepage/FudyShopHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.fudy.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudyShopHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FudyShopHomepageApplication.class, args);
+	}
+
+}
