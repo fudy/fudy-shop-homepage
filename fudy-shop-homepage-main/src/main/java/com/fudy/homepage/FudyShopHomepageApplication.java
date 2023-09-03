a81b366b212f69f308cdc230305e6221d3fce86a
Fudy <lifudong1@163.com>
2023-09-03 22:29:20 +0800

    docker refactor

--- fudy-shop-homepage-main/src/main/java/com/fudy/homepage/FudyShopHomepageApplication.java
+++ fudy-shop-homepage-main/src/main/java/com/fudy/homepage/FudyShopHomepageApplication.java
@@ -1,15 +1,25 @@
 package com.fudy.homepage;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.fudy.homepage"})
 @MapperScan("com.fudy.homepage.infrastructure.mybatis.mapper")
 public class FudyShopHomepageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FudyShopHomepageApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FudyShopHomepageApplication.class, args);
+		ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+		log.info("thread classloader: {}", contextClassLoader);
+		log.info("current classloader:{}", FudyShopHomepageApplication.class.getClassLoader());
+		String[] beanDefinitionNames = context.getBeanDefinitionNames();
+		for (String name : beanDefinitionNames) {
+			log.info("====> found bean, name: {}", name);
+		}
 	}
 
 }
