ebab695f5016a3655ca4b9d4575f526c34382ab6
Fudy <lifudong1@163.com>
2023-09-03 22:30:03 +0800

    docker deploy refactor

---
+++ deploy/bin/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+## define variables
+TARGET_DIR=/opt/docker/fudy-shop-homepage
+TARGET_JAR=../../fudy-shop-homepage-main/target/fudy-shop-homepage.jar
---
+++ fudy-shop-homepage-main/src/main/java/com/fudy/homepage/MainController.java
@@ -0,0 +1,19 @@
+package com.fudy.homepage;
+
+import com.fudy.homepage.interfaces.dto.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public @ResponseBody
+    Result<String> root() {
+        return Result.success("fudy-shop-homepage");
+    }
+}
