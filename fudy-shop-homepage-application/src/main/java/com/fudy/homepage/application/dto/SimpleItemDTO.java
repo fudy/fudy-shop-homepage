84e786346c54996b770000b52a6a449ae89d5a29
Fudy <lifudong1@163.com>
2023-10-01 09:54:04 +0800

    add item id

--- fudy-shop-homepage-application/src/main/java/com/fudy/homepage/application/dto/SimpleItemDTO.java
+++ fudy-shop-homepage-application/src/main/java/com/fudy/homepage/application/dto/SimpleItemDTO.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 @Data
 public class SimpleItemDTO implements Serializable {
     private static final long serialVersionUID = -1752495821105167936L;
+    private Long id;
     /**
      * 商品名称
      */
--- fudy-shop-homepage-domain/src/main/java/com/fudy/homepage/domain/ad/Ad.java
+++ fudy-shop-homepage-domain/src/main/java/com/fudy/homepage/domain/ad/Ad.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Ad {
+    private Long id;
     /**
      * 广告的商品详情链接
      */
