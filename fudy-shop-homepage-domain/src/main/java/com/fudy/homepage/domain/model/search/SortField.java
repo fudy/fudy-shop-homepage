24a02a99642c2c6e37d5d53106e503bcc8503f4c
Fudy <lifudong1@163.com>
2024-03-02 13:59:07 +0800

    fix SortField compile error

--- fudy-shop-homepage-domain/src/main/java/com/fudy/homepage/domain/model/search/SortField.java
+++ fudy-shop-homepage-domain/src/main/java/com/fudy/homepage/domain/model/search/SortField.java
@@ -12,30 +12,7 @@ public enum SortField {
         this.value = value;
     }
 
-    public static SortField of(String str) {package com.fudy.homepage.domain.model.search;
-
-import lombok.Getter;
-
-        @Getter
-        public enum SortField {
-            PRICE("price");
-
-            private String value;
-
-            SortField(String value) {
-                this.value = value;
-            }
-
-            public static com.fudy.homepage.domain.model.search.SortField of(String str) {
-                for (com.fudy.homepage.domain.model.search.SortField f : com.fudy.homepage.domain.model.search.SortField.values()) {
-                    if (f.value.equals(str)) {
-                        return f;
-                    }
-                }
-                return null;
-            }
-        }
-
+    public static SortField of(String str) {
         for (SortField f : SortField.values()) {
             if (f.value.equals(str)) {
                 return f;
